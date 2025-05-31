395a7231d9e60dedee3d21cb36b416547898435d
ellie <dalloc2@hotmail.com>
2025-05-31 19:07:54 +0100

    Update signal.lua
    Maybe it'll work idk

--- signal.lua
+++ signal.lua
@@ -1,4 +1,5 @@
 -- thread/connection handler
+local localThreads = (getgenv().hasName) and getgenv()[hasName] or threads
 local function stopThread(thread)
 	if (typeof(thread) == "table") then
 		return stopThread(thread.thread)
@@ -14,12 +15,14 @@ local function createSignal(connOrThread, name)
 	if (name) then
 		tbl = { name = name, thread = connOrThread }
 	else
-		table.insert(threads, connOrThread)
+		table.insert(localThreads, connOrThread) 
 	end
-	if (tbl) then table.insert(threads, tbl) end
+	if (tbl) then 
+		table.insert(localThreads, tbl) 
+	end
 end
 local function stopSignal(name)
-	for i,v in threads do
+	for i,v in localThreads do
 		if (typeof(v) == "table" and v.name == name) then
 			warn(v)
 			stopThread(v)
@@ -27,11 +30,13 @@ local function stopSignal(name)
 	end
 end
 
-if (threads) then
-	for _,v in threads do
+if (localThreads) then
+	for _,v in localThreads do
 		stopThread(v)
 	end
 end
-getgenv().threads = {}
+if (not getgenv().hasName) then
+	getgenv().threads = {}
+end
 
 return { stopThread = stopThread, stopSignal = stopSignal, createSignal = createSignal }
